693ea6598c042f7be26b4b5fd1edbdc32dadcb54
avrasensharma-cpu <avrasensharma@gmail.com>
2026-05-07 16:23:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('v1').then((cache) => cache.addAll(['./index.html', './manifest.json']))
-  );
+const cacheName = 'royalasia-v18-b';
+const assets = ['./', './rpg.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
